7b9cf6f36da66baebaf2f7748be36b667cecab57
Vitalyi <evciu97@gmail.com>
2023-07-24 22:17:48 +0300

    base

--- main.py
+++ main.py
@@ -36,23 +36,28 @@ async def get_exchange(dates):
 
 def main(date_delta):
     loop = asyncio.get_event_loop()
-    dates = generate_dates(date_delta)
-    for date in dates:
-        dates_list.append(date.strftime("%d.%m.%Y"))
-
     try:
-        res = loop.run_until_complete(get_exchange(dates_list))
-        return res
-    except (aiohttp.ClientError, ValueError) as e:
-        # Обробка помилок мережевих запитів та обробки JSON
-        print(f"Error while fetching data: {e}")
+        date_delta = int(date_delta)
+        if date_delta > 10:
+            print("Too much days, max is 10days")
+        else:
+            dates = generate_dates(date_delta)
+            for date in dates:
+                dates_list.append(date.strftime("%d.%m.%Y"))
+
+            try:
+                res = loop.run_until_complete(get_exchange(dates_list))
+                return res
+            except (aiohttp.ClientError, ValueError) as e:
+                # Обробка помилок мережевих запитів та обробки JSON
+                print(f"Error while fetching data: {e}")
         return
+    except ValueError as e:
+        return f'Please give me a number! Error:{e}'
+        
 
 if __name__ == "__main__":
     date_delta = sys.argv[1]
-    if int(date_delta) > 10:
-        print("Too much days, max is 10days")
-    else:
-        res = main(int(date_delta))
-        print(res)
+    res = main(date_delta)
+    print(res)
     
